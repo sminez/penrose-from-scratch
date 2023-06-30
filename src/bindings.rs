567adfa5808ac981240b83464e93fc72855158ec
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2023-06-30 19:53:25 +0100

    ep07: additional keybindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -13,6 +13,7 @@ use penrose::{
 };
 use std::{collections::HashMap, process::exit};
 
+/// A custom dmenu menu for running actions related to shutdown and logout
 fn power_menu() -> Box<dyn KeyEventHandler<RustConn>> {
     key_handler(|state, _| {
         let screen_index = state.client_set.current_screen().index();
@@ -37,39 +38,51 @@ fn power_menu() -> Box<dyn KeyEventHandler<RustConn>> {
     })
 }
 
+/// Set up for our keybindings including triggering of custom key event
+/// handlers.
+///
+/// The available builtin methods for manipulating the ClientSet can be
+/// found here in the docs:
+///   https://sminez.github.io/penrose/rustdoc/penrose/pure/struct.StackSet.html
 pub fn raw_key_bindings(
     toggle_scratch: ToggleNamedScratchPad,
 ) -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
     let mut raw_bindings = map! {
         map_keys: |k: &str| k.to_string();
 
+        // Client movement
         "M-j" => modify_with(|cs| cs.focus_down()),
         "M-k" => modify_with(|cs| cs.focus_up()),
         "M-S-j" => modify_with(|cs| cs.swap_down()),
         "M-S-k" => modify_with(|cs| cs.swap_up()),
+        "M-space" => modify_with(|cs| cs.swap_focus_and_head()),
+        "M-C-space" => modify_with(|cs| cs.rotate_focus_to_head()),
+
         "M-S-q" => modify_with(|cs| cs.kill_focused()),
 
+        // Workspace movement
         "M-Tab" => modify_with(|cs| cs.toggle_tag()),
         "M-bracketright" => modify_with(|cs| cs.next_screen()),
         "M-bracketleft" => modify_with(|cs| cs.previous_screen()),
         "M-S-bracketright" => modify_with(|cs| cs.drag_workspace_forward()),
         "M-S-bracketleft" => modify_with(|cs| cs.drag_workspace_backward()),
 
+        // Layout control
         "M-grave" => modify_with(|cs| cs.next_layout()),
         "M-S-grave" => modify_with(|cs| cs.previous_layout()),
-
         "M-S-Up" => send_layout_message(|| IncMain(1)),
         "M-S-Down" => send_layout_message(|| IncMain(-1)),
         "M-S-Right" => send_layout_message(|| ExpandMain),
         "M-S-Left" => send_layout_message(|| ShrinkMain),
 
+        // Launchers
         "M-semicolon" => spawn("rofi-apps"),
         "M-Return" => spawn("kitty"),
         "M-slash" => Box::new(toggle_scratch),
-
         "M-A-Escape" => power_menu(),
     };
 
+    // Per-workspace focusing and client throwing
     for tag in &["1", "2", "3", "4", "5", "6", "7", "8", "9"] {
         raw_bindings.extend([
             (
